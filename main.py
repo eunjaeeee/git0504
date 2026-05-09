59eb9ade182bd6e786fe22e95c9eac73233eb916
eunjae <ejp032226@gmail.com>
2026-05-10 03:10:15 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils import add, sub, mul
+
+a = int(input("insert a: "))
+b = int(input("insert b: "))
+str_input = input("insert str:")
+
+print("add", add(a,b))
+print("sub", sub(a,b))
+print("mul", mul(a,b))
---
+++ utils.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def reverse(s):
+    return s[::-1]
+
+def is_even(n):
+    return n % 2 == 0
